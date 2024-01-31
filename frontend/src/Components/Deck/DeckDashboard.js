69e686a446820f43277ea84224bf3136639be959
Austin <auserich@gmail.com>
2024-01-31 10:47:04 -0800

    Added adding/viewing component for decks. Can delete but not edit

--- frontend/src/Components/Deck/DeckDashboard.js
+++ frontend/src/Components/Deck/DeckDashboard.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AppNavbar from "../AppNavbar/AppNavbar";
 import DeckAdder from "./DeckAdder";
+import DeckViewer from "./DeckViewer";
 
 const DeckDashboard = () => {
+	const [decksUpdated, setDecksUpdated] = useState(false);
+
+	const handleDeckAdded = () => {
+		setDecksUpdated(!decksUpdated);
+	};
+
 	return (
 		<>
 			<AppNavbar />
 			<h3>Deck Dashboard</h3>
-			<DeckAdder />
+			<DeckAdder onDeckAdded={handleDeckAdded} />
+			<DeckViewer decksUpdated={decksUpdated} />
 		</>
 	);
 };
